1aeb1d04d45fadc548964a212c30f01edf32f87b
Andrey Pershin <aspershi@gmail.com>
2019-06-05 14:34:01 +0300

    google map continue

--- src/scripts/modules/map.js
+++ src/scripts/modules/map.js
@@ -1,26 +1,22 @@
-  function initMap() {
-    var myLatLng = {lat: -25.363, lng: 131.044};
+function initMap() {
+  var myLatLng = {lat: -25.363, lng: 131.044};
 
   // Create a map object and specify the DOM element
   // for display.
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: myLatLng,
-    zoom: 4
-  });
+  center: myLatLng,
+  zoom: 4
+});
 
-  // Create a marker and set its position.
-  var marker = new google.maps.Marker({
-    map: map,
-    position: myLatLng,
-    title: 'Hello World!'
-  });
+// Create a marker and set its position.
+var marker = new google.maps.Marker({
+  map: map,
+  position: myLatLng,
+  title: 'Hello World!'
+});
 }
 
 global.initMap = initMap;
 window.initMap = initMap;
-
-// scr="https://maps.googleapis.com/maps/api/js?key=AIzaSyALKvZu7DcQHR689o1h-jksRfdK9iE3KmY&callback=initMap"
-
-    // script(async defer src="https://maps.googleapis.com/maps/api/js?key=&callback=initMap")
 
 console.log('hello from map');
